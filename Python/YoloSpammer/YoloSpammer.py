a1d8933a64b2322f96870f168de281f7f2ccab2a
xtreme243 <53234687+xtreme243@users.noreply.github.com>
2020-10-06 09:46:22 -0700

    Create YoloSpammer.py

---
+++ Python/YoloSpammer/YoloSpammer.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from colorama import Fore, Style
+
+
+driver = webdriver.Chrome()#change to PhantomJS() if you don't want browser to open up
+
+
+url = "https://onyolo.com/m/"+input("what is the yolo code(the thing in the URL): ")+"?"
+
+driver.get(url)
+
+text = input("What would you like spammed: ")
+numberOfSpams = int(input("How many times do you want this spammed: "))
+while numberOfSpams!=0:
+    driver.find_element_by_xpath('//*[@id="text"]').send_keys(text)
+    driver.find_element_by_xpath('//*[@id="send-button"]').click()
+    driver.refresh()
+    print(Fore.RED+"--------->Sent")
+    numberOfSpams-=1
+print(Style.RESET_ALL)
